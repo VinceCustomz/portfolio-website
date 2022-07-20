996589a9fd07b2be71e29deb382973fc44142f1f
vincecustomz <vince.vuong@live.com>
2022-07-20 15:00:07 -0400

    swapped projects

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -22,26 +22,25 @@ export const aboutMe = {
   link: "https://docs.google.com/document/d/1Tnn2t1nFRSSCS88WXW1CRcVkYVvc96_5LOOdNXHxGzg/export?format=pdf",
   // export?format=pdf - allows the link to be a downloadable file
 };
-
 export const projectsOne = {
   id: "projectsOne",
   lightBg: !false,
   lightText: !true,
   lightTextDesc: !true,
   topLine: "Projects",
-  headLine: "PokéPacks",
+  headLine: "Plenty of Fish",
   description:
-    "PokéPacks is a web application where you can simulate opening Pokémon Booster packs, as well as track your collection by using the pokemonTCG Api. Remember there was a time, the only thing that mattered was... Pokémon cards!",
+    "Plenty of Fish is a photo-sharing web application deisgned to support users in organizing and sharing the fish they catch. Each entry allows a user to log specific information about their catch, comment/rate on other users entries, and tell mention if that catch was delicious, thus making it a fun experience for all!",
   toolStatement: "Tools used: ",
-  toolsUsed: "React.js, Javascript, Express, MongoDB, Tailwind",
+  toolsUsed: "Python, Django, MongoDB, Tailwind",
   buttonLabel: "See Live",
   imgStart: !true,
-  img: require("../../images/svg-2.png"),
-  alt: "pokepacks",
+  img: require("../../images/svg-3.png"),
+  alt: "plentyfish",
   dark: !true,
   primary: !true,
   darkText: !false,
-  link: "https://pokemontcgtracker3.herokuapp.com/home",
+  link: "https://plentyfish.herokuapp.com/fishes",
 };
 
 export const projectsTwo = {
@@ -50,20 +49,21 @@ export const projectsTwo = {
   lightText: true,
   lightTextDesc: true,
   topLine: "Projects",
-  headLine: "Plenty of Fish",
+  headLine: "PokéPacks",
   description:
-    "Plenty of Fish is a photo-sharing web application deisgned to support users in organizing and sharing the fish they catch. Each entry allows a user to log specific information about their catch, comment/rate on other users entries, and tell mention if that catch was delicious, thus making it a fun experience for all!",
+    "PokéPacks is a web application where you can simulate opening Pokémon Booster packs, as well as track your collection by using the pokemonTCG Api. Remember there was a time, the only thing that mattered was... Pokémon cards!",
   toolStatement: "Tools used: ",
-  toolsUsed: "Python, Django, MongoDB, Tailwind",
+  toolsUsed: "React.js, Javascript, Express, MongoDB, Tailwind",
   buttonLabel: "See Live",
   imgStart: true,
-  img: require("../../images/svg-3.png"),
-  alt: "plentyfish",
+  img: require("../../images/svg-2.png"),
+  alt: "pokepacks",
   dark: true,
   primary: true,
   darkText: false,
-  link: "https://plentyfish.herokuapp.com/fishes",
+  link: "https://pokemontcgtracker3.herokuapp.com/home",
 };
+
 
 export const projectsThree = {
   id: "projectsThree",
